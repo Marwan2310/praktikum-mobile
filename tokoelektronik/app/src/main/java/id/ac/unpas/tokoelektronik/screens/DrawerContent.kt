37918db32bed57af942e7411d8977d7da4aa80f0
MRafliansyah <203040105@mail.unpas.ac.id>
2023-06-15 23:27:01 +0700

    menambahkan icon dimenu

--- tokoelektronik/app/src/main/java/id/ac/unpas/tokoelektronik/screens/DrawerContent.kt
+++ tokoelektronik/app/src/main/java/id/ac/unpas/tokoelektronik/screens/DrawerContent.kt
@@ -2,14 +2,7 @@ package id.ac.unpas.tokoelektronik.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -36,21 +29,26 @@ fun DrawerContent (onClick: (String) -> Unit){
         Menu.SMARTPHONE,
         Menu.ABOUTTEAM,)
 
-        val listState = rememberLazyListState()
-    Column (
-        modifier = Modifier.background(MaterialTheme.colors.background)
+    val listState = rememberLazyListState()
 
+    Column(
+        modifier = Modifier.background(MaterialTheme.colors.background)
     ) {
-        Row (modifier = Modifier
-            .fillMaxWidth()
-            .padding(5.dp),
-            horizontalArrangement = Arrangement.Center) {
-            Text("Selamat Datang", modifier =
-            Modifier
-                .height(32.dp)
-                .padding(2.dp),
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "Elektronik C6",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .height(32.dp),
                 fontSize = 18.sp,
-                fontWeight = FontWeight.SemiBold)
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Black
+            )
         }
 
         Divider(startIndent = 8.dp, thickness = 1.dp, color = Color.Black)
@@ -58,33 +56,41 @@ fun DrawerContent (onClick: (String) -> Unit){
         LazyColumn(
             state = listState,
             contentPadding = PaddingValues(0.dp)
-        ){
-            items(menus) {menu ->
+        ) {
+            items(menus) { menu ->
                 Card(
                     shape = RoundedCornerShape(4.dp),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(5.dp),
-                    contentColor = Color.Black
+                        .padding(8.dp),
+                    backgroundColor = Color(0xFF6200EE),
+                    contentColor = Color.White
                 ) {
                     Row(
                         modifier = Modifier
-                            .padding(5.dp)
+                            .padding(8.dp)
                             .clickable {
                                 onClick(menu.route)
                             }
                     ) {
-
+                        Icon(
+                            painter = painterResource(id = menu.icon),
+                            contentDescription = null,
+                            tint = Color.White,
+                            modifier = Modifier.size(24.dp)
+                        )
                         Text(
                             stringResource(id = menu.title),
-                            modifier = Modifier.height(32.dp).padding(2.dp),
+                            modifier = Modifier
+                                .height(32.dp)
+                                .padding(start = 8.dp),
                             fontSize = 18.sp,
-                            fontWeight = FontWeight.SemiBold
+                            fontWeight = FontWeight.SemiBold,
+                            color = Color.White
                         )
                     }
                 }
             }
         }
-
     }
 }
